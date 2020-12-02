65d6e7e5a6195c844d26ea89f20b86175237e06f
acreations <aaron.wong.home@gmail.com>
2020-12-02 20:33:46 +0100

    Updates

--- 2/answer.py
+++ 2/answer.py
@@ -6,28 +6,50 @@ def parse(data):
     d = data.split()
 
     return {
-        'min': int(d[0].split("-")[0]),
-        'max': int(d[0].split("-")[1]),
+        'a': int(d[0].split("-")[0]),
+        'b': int(d[0].split("-")[1]),
         'policy': d[1][:1],
         'passwd': d[2]
     }
 
-def is_valid(min, max, policy, passwd):
+def is_valid_policy_one(a, b, policy, passwd):
 
     counter = passwd.count(policy)
 
-    return counter >= min and counter <= max
+    return counter >= a and counter <= b
+
+def is_valid_policy_two(a, b, policy, passwd):
+
+    match_pos_one = passwd[a-1] == policy
+    match_pos_two = passwd[b-1] == policy
+
+    return (match_pos_one or match_pos_two) and not (match_pos_one and match_pos_two)
+
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 
 input = open("%s/input" % base_path, "r").read().splitlines()
 
-valid = 0;
+valid_policy_one = 0;
+
+print("\n = Part One\n")
 
 for row in input:
     data = parse(row)
 
-    if is_valid(**data):
-        valid += 1
+    if is_valid_policy_one(**data):
+        valid_policy_one += 1
 
-print("Number of valid passwords are {} (of {} passwords)a".format(valid, len(input)))
+print("Number of valid passwords are {} (of {} passwords)".format(valid_policy_one, len(input)))
+
+print("\n = Part Two\n")
+
+valid_policy_two = 0
+
+for row in input:
+    data = parse(row)
+
+    if is_valid_policy_two(**data):
+        valid_policy_two += 1
+
+print("Number of valid passwords are {} (of {} passwords)".format(valid_policy_two, len(input)))
