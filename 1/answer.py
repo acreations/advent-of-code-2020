29aec914dc39a181aa1f8d12a540391a83399bbd
acreations <aaron.wong.home@gmail.com>
2020-12-04 21:45:35 +0100

    Updates

--- 1/answer.py
+++ 1/answer.py
@@ -2,9 +2,6 @@
 
 import sys
 
-def correct_sum_of_2020(*n):
-    return sum(n) == 2020
-
 i = list(map(int, open("{}/input".format(sys.path[0]), "r").read().split()))
 
 equal_size = int(len(i)/2)
@@ -14,7 +11,7 @@ print(open("{}/part_one".format(sys.path[0]), "r").read())
 
 for x in range(0, equal_size-1):
     for y in range(equal_size, len(i)):
-        if correct_sum_of_2020(i[x],i[y]):
+        if i[x] + i[y] == 2020:
             print("{}\nMy answer is {}\n{}".format("="*20, i[x]*i[y], "="*20))
 
 print("")
@@ -25,5 +22,5 @@ equal_size = int(len(i)/3)
 for x in range(0, equal_size-1):
     for y in range(equal_size, equal_size*2-1):
         for z in range(equal_size, len(i)):
-            if correct_sum_of_2020(i[x],i[y],i[z]):
+            if i[x] + i[y] + i[z] == 2020:
                 print("{}\nMy answer is {}\n{}".format("="*22, i[x]*i[y]*i[z], "="*22))
